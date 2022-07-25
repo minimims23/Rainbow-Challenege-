8c8ac5584119fe4a5fe0dd1d5dc4fd6b9ff54a06
minimims23 <102005539+minimims23@users.noreply.github.com>
2022-07-25 16:39:28 +0200

    Add files via upload

---
+++ rainbow.js
@@ -0,0 +1,31 @@
+
+
+
+function red() {
+  document.getElementById("Red").style.backgroundColor= "red";
+}
+
+function orange() {
+  document.getElementById("Orange").style.backgroundColor= "orange";
+}
+
+function yellow() {
+  document.getElementById("Yellow").style.backgroundColor= "yellow";
+}
+
+function green() {
+  document.getElementById("Green").style.backgroundColor= "green";
+}
+
+function blue() {
+  document.getElementById("Blue").style.backgroundColor= "blue";
+}
+
+function purple() {
+  document.getElementById("Purple").style.backgroundColor= "purple";
+}
+
+function lavender() {
+  document.getElementById("Lavender").style.backgroundColor= "lavender";
+}
+
